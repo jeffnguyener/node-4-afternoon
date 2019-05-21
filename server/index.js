d31d96e15fab201dad7615446ba3fdaaeb49f097
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-21 15:40:17 -0600

    update controllers

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const {SERVER_PORT, SESSION_SECRET} = process.env
+const checkForSession = require('./middlewares/checkForSession')
+const swagController = require('./controllers/swagController')
+const authController = require('./controllers/authController')
+
 const app = express()
 
 app.use(express.json())
 app.use(session({
     secret: SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60 * 60
     }
 }))
+
+app.post('/api/register', authController.register)
+app.post('/api/login', authController.login)
+app.post('/api/signout', authController.signout)
+app.get('/api/user', authController.getUser)
+app.get('/api/swag', swagController.read)
+
+
+app.use(checkForSession)
 
 app.listen(SERVER_PORT, () => console.log(`Running on port ${SERVER_PORT}`))
